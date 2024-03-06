ec964f5b5fc95f72e17310995825e65e63f7fd1b
jlewis47 <lewis@iap.fr>
2024-03-06 11:30:19 +0100

    switched maps to hdf5 files, functions are flexible halo or galaxy tree

--- tree_reader.py
+++ tree_reader.py
@@ -1,16 +1,12 @@
 import numpy as np
 import os
 
-# from gremlin.read_sim_params import ramses_sim
 from f90_tools.IO import read_record, skip_record
 
-# from hagn.utils import get_hagn_sim
+import h5py
 
 
 def map_tree_rev_steps_bytes(fname, out_path, star=False):
-
-    ##this could be made better by writing to binary... also more storage efficient
-    ##will see if I get the time (good luck me!)
 
     if not os.path.exists(out_path):
         os.makedirs(out_path, exist_ok=True)
@@ -38,6 +34,8 @@ def map_tree_rev_steps_bytes(fname, out_path, star=False):
 
             ntot = nb_halos[istep] + nb_shalos[istep]
 
+            # print(ntot)
+
             ids = np.empty(ntot, dtype=np.int32)
             nbytes = np.empty(ntot, dtype=np.int64)
 
@@ -59,6 +57,7 @@ def map_tree_rev_steps_bytes(fname, out_path, star=False):
                 nbytes[iobj] = src.tell()
                 nb_fathers = np.fromfile(src, dtype=np.int32, count=28 + 13 * 2)
                 ids[iobj] = nb_fathers[1]
+                # print(ids[iobj])
                 nb_fathers = nb_fathers[-2]
                 # print(nb_fathers)
                 if nb_fathers > 0:
@@ -75,48 +74,49 @@ def map_tree_rev_steps_bytes(fname, out_path, star=False):
                 if star:
                     skip_record(src, 1, np.int32)
 
-            with open(os.path.join(out_path, f"bytes_step_{istep:d}.txt"), "w") as out:
-                out.write(f"{byte_positions[istep]}\n")
-                for iobj in range(ntot):
-                    out.write(f"{ids[iobj]:d},{nbytes[iobj]:d}\n")
+            with h5py.File(
+                os.path.join(out_path, f"bytes_step_{istep:d}.h5"), "w"
+            ) as out:
+                # out.write(f"{byte_positions[istep]}")
+                out.create_dataset(
+                    "step_nbytes",
+                    data=byte_positions[istep],
+                    dtype=np.int64,
+                )
+                out.create_dataset(
+                    "obj_ids", data=ids, dtype=np.int32, compression="lzf"
+                )
+                out.create_dataset(
+                    "obj_nbytes", data=nbytes, dtype=np.int64, compression="lzf"
+                )
 
 
-def istep_to_nbyte_gal_rev(istep):
+def istep_to_nbyte_rev(istep, tree_type="gal"):
 
-    with open(
-        os.path.join(
-            "/data101/jlewis/hagn/tree_offsets/gal/all_fine_rev",
-            f"bytes_step_{istep:d}.txt",
-        ),
-        "r",
-    ) as src:
-        line = src.readline()
+    fpath = f"/data101/jlewis/hagn/tree_offsets/{tree_type}/all_fine_rev"
 
-    return int(line)
+    with h5py.File(os.path.join(fpath, f"bytes_step_{istep:d}.h5"), "r") as src:
+        return int(src["step_nbytes"][()])
 
 
-def iobj_to_nbyte_gal_rev(istep, obj_id):
+def iobj_to_nbyte_rev(istep, obj_id, tree_type="gal"):
 
-    with open(
-        os.path.join(
-            "/data101/jlewis/hagn/tree_offsets/gal/all_fine_rev",
-            f"bytes_step_{istep:d}.txt",
-        ),
-        "r",
-    ) as src:
-        for i, line in enumerate(src):
-            if i == obj_id:
-                break
+    fpath = f"/data101/jlewis/hagn/tree_offsets/{tree_type}/all_fine_rev"
 
-    return np.int64(line.split(",")[1])
+    with h5py.File(os.path.join(fpath, f"bytes_step_{istep:d}.h5"), "r") as src:
+        # ids = src["obj_ids"][()]
+        # find_line = np.searchsorted(ids, obj_id)
+        return src["obj_nbytes"][obj_id - 1]
 
 
-def read_tree_gal_rev(zstart: float, tgt_ids):
+def read_tree_rev(zstart: float, tgt_ids, tree_type="gal"):
     """
     zstart is the redshift at which to start the tree, code will start from closest avaialble step from tree
     tgt_ids is an iterable containing the ids of the objects to follow, starting at z=zstart
 
     """
+
+    assert tree_type in ["gal", "halo"], "possible types are 'gal' or 'halo'"
 
     # using the offset files to jump to the right byte position,
     # follow the main branch from zstart to the highest possible redshift
@@ -127,22 +127,13 @@ def read_tree_gal_rev(zstart: float, tgt_ids):
     with open(fname, "rb") as src:
 
         nsteps = read_record(src, 1, np.int32)
-        # print(nsteps)
-        # print(read_record(src, 1, np.int32))
-        # print(read_record(src, 1, np.int32))
+
         nb_halos = read_record(src, nsteps * 2, np.int32, debug=False)
-        # nb_halos = np.fromfile(src, dtype=np.int32, count=2 * nsteps + 2)[1:-1]
         nb_halos, nb_shalos = nb_halos[:nsteps], nb_halos[nsteps:]
-        # print(nb_halos, nb_shalos)
         tree_aexps = read_record(src, nsteps, np.float32)
-        # tree_omega_t = read_record(src, nsteps, np.float32)
-        # tree_age_univ = read_record(src, nsteps, np.float32)
         skip_record(src, nsteps, np.float32)
         skip_record(src, nsteps, np.float32)
 
-        # print(nb_halos, nb_shalos, tree_aexps, tree_omega_t, tree_age_univ)
-
-        # skip = tree_aexps > (1.0 / (1.0 + zstart))
         skip = np.argmin(np.abs(tree_aexps - (1.0 / (1.0 + zstart))))
 
         found_ids = np.full((len(tgt_ids), nsteps - skip), -1, dtype=np.int32)
@@ -150,66 +141,28 @@ def read_tree_gal_rev(zstart: float, tgt_ids):
 
         found_ids[:, 0] = tgt_ids
 
-        # print(nb_halos, nb_shalos, tree_aexps, tree_omega_t, tree_age_univ)
-
-        # print(nsteps - skip)
-
         for istep in range(skip, nsteps):
 
-            nyte_skip = istep_to_nbyte_gal_rev(istep)
+            nyte_skip = istep_to_nbyte_rev(istep, tree_type=tree_type)
             src.seek(nyte_skip)
-            # ntot = nb_halos[istep] + nb_shalos[istep]
-            # print(f"istep: {istep:d} has {ntot:d} halos+subhalos")
-            print(f"step {istep}, z={1.0 / tree_aexps[istep] - 1.0:.4f}")
 
-            # print("byte position is ", src.tell())
-            # we aren't at the first step, and didn't find any ids in the previous step... stop the tree
+            # print(f"step {istep}, z={1.0 / tree_aexps[istep] - 1.0:.4f}")
+
             if istep > skip and np.all(found_ids[:, istep - skip - 1] == -1):
                 print("stopping tree... no fathers found in previous step")
                 break
 
-            # nfound_this_step = 0
-
-            # for iobj in range(ntot):
-
             for iobj in np.sort(found_ids[:, istep - skip]):
 
-                obj_bytes = iobj_to_nbyte_gal_rev(istep, iobj)
+                obj_bytes = iobj_to_nbyte_rev(istep, iobj, tree_type=tree_type)
 
                 src.seek(obj_bytes)
 
-                # print("iobj:", iobj)
-
                 mynumber = read_record(src, 1, np.int32)
-                # # print(mynumber)
-                # bushID = read_record(src, 1, np.int32)
-                # # # print(bushID)
-                # mystep = read_record(src, 1, np.int32) - 1  # py indexing
-                # # # print(mystep)
-                # level, hosthalo, hostsub, nbsub, nextsub = read_record(
-                #     src, 5, np.int32, debug=False
-                # )
 
                 np.fromfile(src, dtype=np.int32, count=7 + 3 * 2)
 
-                # print(level, hosthalo, hostsub, nbsub, nextsub)
                 m = read_record(src, 1, np.float32)
-                # print("%e" % (m * 1e11))
-                # macc = read_record(src, 1, np.float32)
-                # # print(macc)
-                # px, py, pz = read_record(src, 3, np.float32)
-                # # print(px, py, pz)
-                # vx, vy, vz = read_record(src, 3, np.float32)
-                # # print(vx, vy, vz)
-                # Lx, Ly, Lz = read_record(src, 3, np.float32)
-                # # print(Lx, Ly, Lz)
-                # r, ra, rb, rc = read_record(src, 4, np.float32)
-                # # print(r, ra, rb, rc)
-                # ek, ep, et = read_record(src, 3, np.float32)
-                # # print(ek, ep, et)
-                # spin = read_record(src, 1, np.float32)
-                # # print(spin)
-
                 np.fromfile(
                     src, dtype=np.int32, count=18 + 7 * 2
                 )  # quickly skip all these entries
@@ -221,18 +174,6 @@ def read_tree_gal_rev(zstart: float, tgt_ids):
                     id_fathers = read_record(src, nb_fathers, np.int32, debug=False)
                     m_fathers = read_record(src, nb_fathers, np.float32)
 
-                    # print(m_fathers)
-                    # print(id_fathers.shape, m_fathers.shape)
-
-                    # print(
-                    #     mynumber,
-                    #     found_ids[:, istep - skip],
-                    # )
-
-                    # assert (
-                    #     mynumber in found_ids[:, istep - skip]
-                    # ), "Error... didn't find expected id at coordinates... byte map file likely wrong or mistmatched"
-
                     if mynumber in found_ids[:, istep - skip]:
 
                         found_arg = np.where(found_ids[:, istep - skip] == mynumber)[0]
@@ -242,86 +183,16 @@ def read_tree_gal_rev(zstart: float, tgt_ids):
                         if nb_fathers > 1:  # if several follow main branch
                             massive_father = np.argmax(m_fathers)
 
-                            # print(massive_father)
-
                             main_id = id_fathers[massive_father]
-                            # main_m = m_fathers[massive_father]
 
-                            # print(list(zip(id_fathers, m_fathers * 1e11)))
                         else:
 
                             main_id = id_fathers
-                            # main_m = m_fathers
-                            # print(id_fathers, m_fathers * 1e11)
-
-                        # print(found_arg, np.sum(found_ids[:, istep - skip] == mynumber))
 
                         if istep < nsteps - 1:
                             found_ids[found_arg, istep - skip + 1] = main_id
-                        # found_masses[found_arg, istep - skip + 1] = main_m * 1e11
-
-                        # print(
-                        # mynumber,
-                        # found_ids[found_arg, istep - skip + 1],
-                        # found_masses[found_arg, istep - skip + 1],
-                        # )
-
-                        # nfound_this_step += 1
-
-                        # if nfound_this_step == len(
-                        #     tgt_ids
-                        # ):  # if I found all the ids at this step... skip to next
-                        #     # skip_bytes = zed_to_nbyte(
-                        #     #     fbytes, 1.0 / tree_aexps[istep - skip + 1] - 1.0
-                        #     # )
-                        #     # src.seek(skip_bytes)
-                        #     break  # get out of object id
-
-                # since we're skipping exactly to galaxy/halo positions in the tree file, we don't even need to read all of each entry
-                # in this case we're going in reverse order so we don't care about the suns at all!
-                # nb_sons = read_record(src, 1, np.int32)
-
-                # # print(nb_sons)
-
-                # if nb_sons > 0:
-                # id_sons = read_record(src, nb_sons, np.int32)
-                # skip_record(src, nb_sons, np.int32)
-
-                #     # print(id_sons)
-
-                # # print(mynumber, id_fathers, id_sons)
-
-                # skip_record(src, 1, np.int32)
-                # skip_record(src, 1, np.int32)
-                # if star:
-                #     skip_record(src, 1, np.int32)
 
     return found_ids, found_masses, tree_aexps[skip:]
-
-
-# sim = get_hagn_sim()
-
-# start_z = 2
-# end_z = 15
-
-# target_hid = 147479
-
-# snap_nbs = sim.snap_numbers
-# snap_aexp = sim.get_snap_exps(param_save=False)
-# snap_zeds = 1.0 / snap_aexp - 1.0
-# snap_times = sim.get_snap_times(param_save=False)
-
-# start_arg = np.argmin(np.abs(snap_zeds - start_z))
-# end_arg = np.argmin(np.abs(snap_zeds - end_z))
-
-# snap_nbs = snap_nbs[start_arg:end_arg]
-# snap_aexp = snap_aexp[start_arg:end_arg]
-# snap_zeds = snap_zeds[start_arg:end_arg]
-# snap_times = snap_times[start_arg:end_arg]
-
-# snap_hids = np.empty(len(snap_nbs), dtype=int)
-# snap_masses = np.empty(len(snap_nbs), dtype=float)
-# snap_rads = np.empty(len(snap_nbs), dtype=float)
 
 
 # map_tree_rev_steps_bytes(
